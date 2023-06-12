946caea57a37380b28b8c74cc5bed1c86a3cdfc3
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-12 18:42:48 +0200

    adjust array

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
+console.log(`JS OK`);
+
 const list =document.getElementById(`list`);
+
 
 partners = [
     {
-        name = `Wayne Barnett`,
-        position = `Founder & CEO`,
-        img = `wayne-barnett-founder-ceo.jpg`
-    }
+        completeName : `Wayne Barnett`,
+        position : `Founder & CEO`,
+        img : `wayne-barnett-founder-ceo.jpg`
+    },
     {
-        name = `Angela Caroll`,
-        position = `Chief Editor`,
-        img = `angela-caroll-chief-editor.jpg`
-    }
+        completeName : `Angela Caroll`,
+        position : `Chief Editor`,
+        img : `angela-caroll-chief-editor.jpg`
+    },
     {
-        name = `Walter Gordon`,
-        position = `Office Manager`,
-        img = `walter-gordon-office-manager.jpg`
-    }
+        completeName : `Walter Gordon`,
+        position : `Office Manager`,
+        img : `walter-gordon-office-manager.jpg`
+    },
     {
-        name = `Angela Lopez`,
-        position = `Social Media Manager`,
-        img = `angela-lopez-social-media-manager.jpg`
-    }
+        completeName : `Angela Lopez`,
+        position : `Social Media Manager`,
+        img : `angela-lopez-social-media-manager.jpg`
+    },
     {
-        name = `Scott Estrada`,
-        position = `Developer`,
-        img = `scott-estrada-developer.jpg`
-    }
+        completeName : `Scott Estrada`,
+        position : `Developer`,
+        img : `scott-estrada-developer.jpg`
+    },
     {
-        name = `Barbara Ramos`,
-        position = `Graphic Designer`,
-        img = `barbara-ramos-graphic-designer.jpg`
+        completeName : `Barbara Ramos`,
+        position : `Graphic Designer`,
+        img : `barbara-ramos-graphic-designer.jpg`
     }
 ]
+
+const partnersList;
+
+for(let i = 0 ; i < partners.length ; i++){
+    const partner = partners[i];
+    console.table(partner);
+}
