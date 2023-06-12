dd8bef6833492383f9620a9ec612618b28aa7efa
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-12 18:32:21 +0200

    array done

---
+++ script.js
@@ -0,0 +1,34 @@
+const list =document.getElementById(`list`);
+
+partners = [
+    {
+        name = `Wayne Barnett`,
+        position = `Founder & CEO`,
+        img = `wayne-barnett-founder-ceo.jpg`
+    }
+    {
+        name = `Angela Caroll`,
+        position = `Chief Editor`,
+        img = `angela-caroll-chief-editor.jpg`
+    }
+    {
+        name = `Walter Gordon`,
+        position = `Office Manager`,
+        img = `walter-gordon-office-manager.jpg`
+    }
+    {
+        name = `Angela Lopez`,
+        position = `Social Media Manager`,
+        img = `angela-lopez-social-media-manager.jpg`
+    }
+    {
+        name = `Scott Estrada`,
+        position = `Developer`,
+        img = `scott-estrada-developer.jpg`
+    }
+    {
+        name = `Barbara Ramos`,
+        position = `Graphic Designer`,
+        img = `barbara-ramos-graphic-designer.jpg`
+    }
+]
